38290e3f779dafe882b26bbb90d8e1459059e9a1
a18wilis <a18wilis@student.his.se>
2019-04-16 15:38:31 +0200

    Döpte om projektet och kommenterade koden. Slutgiltig Commit

--- app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
+++ app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
@@ -25,18 +25,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Lägger samman den inkluderade bergdatan och adderar dem till arraylistan om berg
         for(int i = 0; i < mountainNames.length; i++){
             mountainArrayList.add(new Mountain(mountainNames[i],mountainLocations[i],mountainHeights[i]));
         }
 
+        //Om man vill lägga till ett berg manuell får man göra det här med följande kodrad
+        //mountainArrayList.add(new Mountain("namn","plats","höjd"));
+
+        //Skapar en arrayadapter som lägger ihop ListViewen från list_item_textview och inställningarna från activity_main med datan som ska användas (ArrayAdaptern)
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item_textview, R.id.list_item_textview, listData);
         ListView my_listview=(ListView)findViewById(R.id.my_listview);
         my_listview.setAdapter(adapter);
 
+        //Om man klickar på ett berg i listan så dyker en toast innehållande information om det berger upp på skärmen
         my_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String text = new String("Toast");
+
+                //Hämtar informationen som skapas i Mountain-klasses och visar den som en toast
                 Toast.makeText(getApplicationContext(), mountainArrayList.get(position).info(), Toast.LENGTH_SHORT).show();
             }
         });
--- app/src/main/java/org/brohede/marcus/listviewapp/Mountain.java
+++ app/src/main/java/org/brohede/marcus/listviewapp/Mountain.java
@@ -1,34 +1,28 @@
 package org.brohede.marcus.listviewapp;
 
 public class Mountain {
+    //Variabler som används i klassen
     private String name;
     private String location;
     private int height;
 
-    //Konstuktor
+    //Enkel konstruktor
     public Mountain(){
         name="Saknar namn";
         location="Saknar plats";
         height=-1;
     }
-
+    //Konstruktor som kräver inmatande av data som ska användas (namn (n), plats (l) och höjd (h))
     public Mountain(String n, String l, int h){
         name = n;
         location = l;
         height = h;
     }
 
+    //Skapar en sträng som visar information om ett specifikt berg. Informationssträngen skapas för samtliga berg som konsturerats
     public String info(){
         String tmp = new String();
         tmp+=name+" is located in mountain range "+location+" and reaches "+height+"m above sea level.";
         return tmp;
-    }
-
-    public void setName(String n){
-        name = n;
-    }
-
-    public String getName(){
-        return name;
     }
 }
